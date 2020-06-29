eed6fd0589404e57349d75acf64eac23b89ba034
Emily Xie <emilyxxie@gmail.com>
2020-06-29 19:31:34 -0400

    Use dotenv to rope in environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-      filename: "./index.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = () => {
+  /* 
+    Since dotenv requires an environment for variable storage, it only works out of the box 
+    server-side. Thus, we'll need to turn out module.exports = {} into a function and
+    grab the contents of the .env file, turn it into an object, and pass it to Webpack's
+    DefinePlugin so that we can access the variables in the app under 
+    "process.env".
+
+    Solution from Stack Overflow: 
+    https://stackoverflow.com/questions/54169021/cant-access-variable-key-inside-env-dotenv-package-react
+  */
+  const env = dotenv.config().parsed;
+  const envVars = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    // fs: empty from issue: https://github.com/motdotla/dotenv/issues/233
+    node: {
+      fs: "empty"
+    },
+    plugins: [
+      // Using DefinePlugin to access dotenv variables
+      new webpack.DefinePlugin(envVars),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+        filename: "./index.html"
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
         }
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+      ]
+    }
   }
 };
 
